f8e6f736f874fda3713b77d1f3395084f85bf32b
manasa1508 <manasaadusumilli15@gmail.com>
2024-03-05 10:36:40 +0530

    present status

--- src/app/student/page.js
+++ src/app/student/page.js
@@ -1,7 +1,6 @@
- // StudentSearch.js
 "use client"
 import React, { useState, useEffect } from 'react';
-import { Button, ButtonGroup, Table, Pagination, Datepicker } from 'flowbite-react';
+import { Button, ButtonGroup, Table, Pagination, Datepicker, Badge } from 'flowbite-react';
 
 const StudentSearch = () => {
   const [attendanceData, setAttendanceData] = useState([]);
@@ -14,53 +13,82 @@ const StudentSearch = () => {
   useEffect(() => {
     fetchData();
   }, [selectedClass, selectedDate, username]);
-const fetchData = async () => {
-  try {
-    let requestBody = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        className: selectedClass === 'All' ? undefined : selectedClass,
-        username,
-      }),
-    };
 
-    // Include date in the request body if selectedDate is not empty
-    if (selectedDate) {
-      requestBody.body.date = new Date(selectedDate).toISOString().split('T')[0];
+  const fetchData = async () => {
+    try {
+      let requestBody = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          className: selectedClass === 'All' ? undefined : selectedClass,
+          username,
+        }),
+      };
+
+      // Include date in the request body if selectedDate is not empty
+      if (selectedDate) {
+        requestBody.body.date = new Date(selectedDate).toISOString().split('T')[0];
+      }
+
+      const response = await fetch('/api/search', requestBody);
+      const data = await response.json();
+      setAttendanceData(data);
+    } catch (error) {
+      console.error('Error fetching attendance data:', error);
     }
+  };
 
-    const response = await fetch('/api/search', requestBody);
-    const data = await response.json();
-    setAttendanceData(data);
-  } catch (error) {
-    console.error('Error fetching attendance data:', error);
-  }
-};
-  
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
-    const millisecondsToHours = (milliseconds) => {
+  const millisecondsToHours = (milliseconds) => {
     return (milliseconds / (1000 * 60 * 60)).toFixed(2);
   };
+
+  const isPresent = (totalTime) => {
+    const halfTime = 4; // Assuming half time is 4 hours
+    const totalTimeHours = millisecondsToHours(totalTime);
+    return totalTimeHours >= halfTime;
+  };
+
+  const getStatusBadgeStyle = (isPresent) => {
+    if (isPresent) {
+      return {
+        background: 'rgb(145 254 159 / 47%)',
+        color: 'green',
+        boxShadow: '0px 13px 20px 0px #80808029',
+      };
+    } else {
+      return {
+        background: '#ffadad8f',
+        color: 'red',
+        boxShadow: '0px 13px 20px 0px #80808029',
+      };
+    }
+  };
+  
   const renderAttendanceRows = () => {
     return attendanceData.map((entry, index) => (
       <tr key={index}>
         <td>{entry.user_name}</td>
         <td>{millisecondsToHours(entry.total_time)}</td>
+        <td>
+          <Badge style={getStatusBadgeStyle(isPresent(entry.total_time))}>
+            {isPresent(entry.total_time) ? 'Present' : 'Absent'}
+          </Badge>
+        </td>
       </tr>
     ));
   };
 
   return (
     <div className="container text-center mt-5 align-items-center">
-      <h2>Search Attendance</h2>
+      <h2 style={{ fontSize: '24px', color: '#333' }}>Search Attendance</h2>
       <div className="d-flex justify-content-between align-items-center">
-        <ButtonGroup>
+      <ButtonGroup>
           {/* Button group for class selection */}
           <Button onClick={() => setSelectedClass('All')} className={selectedClass === 'All' ? 'active' : ''}>All</Button>
           <Button onClick={() => setSelectedClass('Elite')} className={selectedClass === 'Elite' ? 'active' : ''}>Elite</Button>
@@ -87,12 +115,13 @@ const fetchData = async () => {
       
       {attendanceData.length > 0 && (
         <div className="mt-5">
-          <h2>Attendance Details:</h2>
-          <Table>
+          <h2 style={{ fontSize: '20px', color: '#555' }}>Attendance Details:</h2>
+          <Table style={{ width: '40%', margin: '0 auto' }}> {/* Adjusted table width and center alignment */}
             <thead>
               <tr>
-                <th>Roll Number</th>
-                <th>Total Time (hours)</th>
+                <th style={{ width: '33.33%' }}>Roll Number</th> {/* Adjusted column width */}
+                <th style={{ width: '33.33%' }}>Total Time (hours)</th> {/* Adjusted column width */}
+                <th style={{ width: '0.5%' }}>Status</th> {/* Adjusted column width */}
               </tr>
             </thead>
             <tbody>
@@ -109,5 +138,6 @@ const fetchData = async () => {
     </div>
   );
 };
+
 
 export default StudentSearch;
